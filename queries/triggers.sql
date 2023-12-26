2654dbae5f9292e927440cb0d99ef5c6c63bd24c
barisgirayakman <girayakman@gmail.com>
2023-12-26 22:47:39 +0300

    added one more trigger

--- queries/triggers.sql
+++ queries/triggers.sql
@@ -14,3 +14,17 @@ AS
 	WHERE @EmployeeId=EmployeeID
 
  END;
+
+CREATE TRIGGER trg_AfterOrderInsert
+ON MyOrder
+AFTER INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Update the Customer's budget by subtracting the OrderAmount from CustomerBudget
+    UPDATE Customer
+    SET CustomerBudget = CustomerBudget - i.OrderAmount
+    FROM Customer c
+    INNER JOIN inserted i ON c.CustomerID = i.CustomerID;
+END;
