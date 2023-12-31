1cbffe532906677124f46d989a26c5e1b5623701
barisgirayakman <girayakman@gmail.com>
2023-12-31 12:52:52 +0300

    combined triggers

--- queries/triggers.sql
+++ queries/triggers.sql
@@ -5,26 +5,34 @@ AS
  BEGIN
 	
 	declare @EmployeeId INT;
-	SELECT @EmployeeId=EmployeeID FROM inserted
+	SELECT @EmployeeId=i.EmployeeID FROM inserted i
 
-	
 	UPDATE SalesPerson
 	SET 
 	ProductSold = ProductSold +1
 	WHERE @EmployeeId=EmployeeID
 
- END;
+    IF(NOT EXISTS(SELECT * FROM Employee WHERE @EmployeeId=EmployeeID))
+    BEGIN
+        RAISERROR('Employee not found.', 16, 1)	
+        ROLLBACK TRANSACTION
+    END
+    ELSE
+    BEGIN 
+        -- Update the Customer's budget by subtracting the OrderAmount from CustomerBudget
+        UPDATE Customer
+        SET CustomerBudget = CustomerBudget - p.ProductPrice
+        FROM Customer c
+		INNER JOIN inserted i ON i.CustomerID = c.CustomerID
+		INNER JOIN Product p ON i.ProductID=p.ProductID
 
-CREATE TRIGGER trg_AfterOrderInsert
-ON MyOrder
-AFTER INSERT
-AS
-BEGIN
-    SET NOCOUNT ON;
-
-    -- Update the Customer's budget by subtracting the OrderAmount from CustomerBudget
-    UPDATE Customer
-    SET CustomerBudget = CustomerBudget - i.OrderAmount
-    FROM Customer c
-    INNER JOIN inserted i ON c.CustomerID = i.CustomerID;
+        DECLARE @OrderPrice INT, @CustomerBudget INT
+        SET @CustomerBudget = (SELECT c.CustomerBudget FROM Customer c)
+        SET @OrderPrice = (SELECT p.ProductPrice FROM inserted i INNER JOIN Product p ON i.ProductID=p.ProductID)
+        IF(@OrderPrice>@CustomerBudget)
+        BEGIN
+            RAISERROR ('Customer Budget is insufficient for the order price.', 16, 1);
+            ROLLBACK TRANSACTION
+        END
+    END
 END;
