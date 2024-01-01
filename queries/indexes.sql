5063c75ccf61e3f6f828e55b9105ab30b10932c8
barisgirayakman <girayakman@gmail.com>
2024-01-01 20:35:22 +0300

    updated indexes

--- queries/indexes.sql
+++ queries/indexes.sql
@@ -2,3 +2,13 @@ CREATE INDEX idx_customer
 ON Customer(CustomerID);
 
 
+CREATE INDEX idx_salesperson
+ON SalesPerson(EmployeeID)
+
+
+
+CREATE INDEX idx_service
+ON MyService(ServiceID)
+
+CREATE INDEX idx_bcs
+ON BeautyCareSpecialist(EmployeeID)
