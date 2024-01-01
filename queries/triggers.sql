410f027efa8c94f35eb90da53d88af984c531d24
barisgirayakman <girayakman@gmail.com>
2024-01-01 19:26:22 +0300

    fixed minor issue in trigger

--- queries/triggers.sql
+++ queries/triggers.sql
@@ -20,15 +20,18 @@ AS
     ELSE
     BEGIN 
         -- Update the Customer's budget by subtracting the OrderAmount from CustomerBudget
-        UPDATE Customer
-        SET CustomerBudget = CustomerBudget - p.ProductPrice
-        FROM Customer c
-		INNER JOIN inserted i ON i.CustomerID = c.CustomerID
-		INNER JOIN Product p ON i.ProductID=p.ProductID
+
 
         DECLARE @OrderPrice INT, @CustomerBudget INT
-        SET @CustomerBudget = (SELECT c.CustomerBudget FROM Customer c)
-        SET @OrderPrice = (SELECT p.ProductPrice FROM inserted i INNER JOIN Product p ON i.ProductID=p.ProductID)
+        SET @CustomerBudget = (SELECT TOP 1 c.CustomerBudget FROM Customer c INNER JOIN inserted i ON c.CustomerID = i.CustomerID)
+        SET @OrderPrice = (SELECT TOP 1 p.ProductPrice FROM inserted i INNER JOIN Product p ON i.ProductID=p.ProductID)
+
+
+        UPDATE Customer
+        SET CustomerBudget = CustomerBudget - @OrderPrice
+        FROM Customer c INNER JOIN 
+        inserted i ON i.CustomerID=c.CustomerID
+
         IF(@OrderPrice>@CustomerBudget)
         BEGIN
             RAISERROR ('Customer Budget is insufficient for the order price.', 16, 1);
